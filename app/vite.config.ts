212e5d80b85b64b6d9018125c1af533d4243254e
BenRey <br@massa.net>
2024-07-10 16:38:04 +0200

    init project

---
+++ app/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  // loadEnv(mode, process.cwd()) will load the .env files depending on the mode
+  // import.meta.env.VITE_BASE_APP available here with: process.env.VITE_BASE_APP
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react(), svgr()],
+    server: {
+      fs: {
+        // to allow server ui kit asset like font files
+        allow: ['../..'],
+      },
+    },
+    optimizeDeps: {
+      include: ['react-dom', 'dot-object', 'copy-to-clipboard'],
+    },
+  });
+};
