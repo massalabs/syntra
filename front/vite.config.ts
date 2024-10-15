2d3db96a893e113fe0d23b2958e681e8cbd64577
Pierre Seznec <petterseznec@gmail.com>
2024-10-15 12:14:30 +0200

    update massa-web3 stack

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills, PolyfillOptions } from 'vite-plugin-node-polyfills';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { visualizer } from 'rollup-plugin-visualizer';
 
@@ -10,8 +10,21 @@ export default ({ mode }) => {
   // import.meta.env.VITE_BASE_APP available here with: process.env.VITE_BASE_APP
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
+  // see https://github.com/davidmyersdev/vite-plugin-node-polyfills/issues/81#issuecomment-2325104572
+  const nodePolyfillsFix = (options?: PolyfillOptions | undefined): PluginOption => {
+    return {
+      ...nodePolyfills(options),
+      resolveId(source: string) {
+        const m = /^vite-plugin-node-polyfills\/shims\/(buffer|global|process)$/.exec(source)
+        if (m) {
+          return `node_modules/vite-plugin-node-polyfills/shims/${m[1]}/dist/index.cjs`
+        }
+      }
+    }
+  }
+
   return defineConfig({
-    plugins: [nodePolyfills(), react(), ViteImageOptimizer(), visualizer()],
+    plugins: [nodePolyfillsFix(), react(), ViteImageOptimizer(), visualizer()],
     server: {
       fs: {
         // to allow server ui kit asset like font files
