a31a322a9ef38f37e453f59107cbde31f78dbb5b
Peterjah <ps@massa.net>
2024-12-20 11:53:53 +0100

    add baseline

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ArrowButton } from '@/components/ArrowButton';
@@ -23,15 +24,26 @@ export default function HomePage() {
   };
 
   return (
-    <div className="">
-      <div className="main h-screen">
+    <div className="flex flex-col items-center">
+      <div className="main flex flex-col w-full">
         <NavBar />
+        <div className="p-6 bg-white text-center text-gray-800 bg-opacity-65 rounded-lg shadow-lg my-6 max-w-2xl mx-auto">
+          <p className="text-lg font-semibold">
+            Syntra is a cutting-edge dApp powered by Massa's autonomous smart
+            contracts, designed to simplify token scheduling like never before.
+          </p>
+          <p className="text-lg mt-2">
+            Whether you're tipping content creators or managing token vesting,
+            Syntra ensures seamless, automated transactions.
+          </p>
+        </div>
         <div className="flex flex-col justify-center items-center gap-10 h-full">
           <ScheduleForm />
           <ArrowButton onClick={scrollToList} />
         </div>
       </div>
-      <div ref={scheduleTableRef} className="h-screen  bg-white">
+
+      <div ref={scheduleTableRef} className="h-screen bg-white w-full my-6 ">
         <ScheduleTablesSection />
       </div>
     </div>
